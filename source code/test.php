2e8627f378bbf394bfc22feea11e1a953f92a023
Christian Hur <chrisvhur@gmail.com>
2022-05-18 13:39:06 -0500

    Add files via upload

---
+++ source code/test.php
@@ -0,0 +1,18 @@
+<?php
+define("PI",31);
+	 $vehicle = "Ford";
+	 echo '<p>My dad is driving a 1989 $vehicle Mustang '. PI.'</p>';
+
+
+//echo $_SERVER["SCRIPT_NAME"] ;
+//echo $_SERVER["SERVER_SOFTWARE"];
+//echo $_SERVER["SERVER_PROTOCOL"];
+//echo __FILE__;
+
+//echo __LINE__;
+
+//echo (dirname(__FILE__));
+
+echo 'My username is ' .$_ENV["USER"] . '!';
+
+?>
